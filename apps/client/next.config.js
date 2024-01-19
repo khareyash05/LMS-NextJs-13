c58b37f7bd8182bdd26c7f2781cd2f55289537c1
Yash Khare <yash2010118@akgec.ac.in>
2024-01-19 11:50:32 +0530

    default push

--- apps/client/next.config.js
+++
@@ -1,7 +0,0 @@
-/** @type {import('next').NextConfig} */
-const nextConfig = {
-  reactStrictMode: true,
-  transpilePackages: ["@repo/ui","@repo/db","@repo/api","@repo/auth"]
-}
-
-module.exports = nextConfig
